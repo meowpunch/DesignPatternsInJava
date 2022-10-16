3c6f3be11083911bd3cce8ab51188953ad42cb7c
jinhoon.bae <jinhoonbae@gmail.com>
2022-10-16 15:06:57 +0200

    future, asynchronous processing

---
+++ src/main/java/concurrency/future/FutureEx.java
@@ -0,0 +1,38 @@
+package concurrency.future;
+
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class FutureEx {
+
+  public static void main(String[] args) throws InterruptedException, ExecutionException {
+    log.debug("Enter");
+    final ExecutorService executorService = Executors.newCachedThreadPool();
+
+    final long startTime = System.currentTimeMillis();
+    Future<String> future = executorService.submit(
+        () -> {
+          log.debug("Processing task asynchronously");
+          try {
+            Thread.sleep(2000);
+          } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+          }
+
+          return "AsyncResult";
+        }
+    );
+
+    log.debug("Is the async task done? " + future.isDone());
+    log.debug("Processing the other task");
+    Thread.sleep(2000);
+
+    log.debug("Is the async task done? " + future.isDone());
+    log.debug("The async result is " + future.get()); // blocking, wait the async result.
+    log.debug("Exit " + (System.currentTimeMillis() - startTime));
+  }
+}
--- src/main/java/concurrency/reactive/IntervalEx.java
+++ src/main/java/concurrency/reactive/IntervalEx.java
@@ -1,6 +1,5 @@
 package concurrency.reactive;
 
-import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
