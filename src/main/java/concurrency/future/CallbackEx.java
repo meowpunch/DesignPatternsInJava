c65639e926d8128c6108465dbec9e684a9f6539c
jinhoon.bae <jinhoonbae@gmail.com>
2022-10-17 22:11:40 +0200

    future to callback

---
+++ src/main/java/concurrency/future/CallbackEx.java
@@ -0,0 +1,14 @@
+package concurrency.future;
+
+public class CallbackEx {
+
+  interface Callback {
+    void onSuccess();
+  }
+
+  public static void main(String[] args) {
+
+
+  }
+
+}
--- src/main/java/concurrency/future/FutureTaskEx.java
+++ src/main/java/concurrency/future/FutureTaskEx.java
@@ -4,17 +4,18 @@ import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
+import java.util.concurrent.FutureTask;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
-public class FutureEx {
+public class FutureTaskEx {
 
   public static void main(String[] args) throws InterruptedException, ExecutionException {
     log.debug("Enter");
     final ExecutorService executorService = Executors.newCachedThreadPool();
 
     final long startTime = System.currentTimeMillis();
-    Future<String> future = executorService.submit(
+    FutureTask<String> future = new FutureTask<>(
         () -> {
           log.debug("Processing task asynchronously");
           try {
@@ -25,14 +26,23 @@ public class FutureEx {
 
           return "AsyncResult";
         }
-    );
+    ) {
+      @Override
+      protected void done() {
+        try {
+          System.out.println("The async result is " + get());
+        } catch (InterruptedException | ExecutionException e) {
+          throw new RuntimeException(e);
+        }
+      }
+    };
+
+    executorService.execute(future);
+    executorService.shutdown();
 
     log.debug("Is the async task done? " + future.isDone());
     log.debug("Processing the other task");
     Thread.sleep(1000);
-
-    log.debug("Is the async task done? " + future.isDone());
-    System.out.println("The async result is " + future.get()); // blocking, wait the async result.
     log.debug("Exit " + (System.currentTimeMillis() - startTime));
   }
 }
